1ed80d7ab010a76d44bccc88970a39b458c2cece
Chase Wood <wood.c.chase@gmail.com>
2019-06-05 12:03:18 -0600

    Moved knexfile to root directory

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,18 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://postgres:Safe@localhost/airbnb_clone_tour',
     migrations: {
-      directory: __dirname + '/database/migrations',
-      seeds: __dirname + '/seeds/seed.js'
+      directory: __dirname + '/server/database/migrations',
+      seeds: __dirname + '/server/'
     }
   },
   production: {
     client: 'pg',
     connection: 'http://airbnb-tour.c13ufv3uqugk.us-west-1.rds.amazonaws.com/',
     migrations: {
-      directory: __dirname + '/database/migrations',
-      seeds: __dirname + '/seeds/seed.js'
+      directory: __dirname + '/server/database/migrations',
+      seeds: __dirname + '/server/seeds/seed.js'
     }
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
+const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 const express = require('express');
 const bodyParser = require('body-parser');
